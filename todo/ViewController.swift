b54e7ab848182d87c1e5f0d7a78a98eccd3c8cfb
Demian Basistyi <basicdemich@gmail.com>
2024-12-13 18:16:44 -0600

    Initial Commit

---
+++ todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Demian Basistyi on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
